f11216e5a7c52b581331e78e05bd8acdc1bbeae4
Shakthivel <rn.shakthivel@gmail.com>
2021-11-25 13:44:36 -0800

    Front End: Redux Store Added and Navigationbar Done

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,8 +1,24 @@
+import React, { Suspense } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+
+const LandingPage = React.lazy(() => import('./components/LandingPage/LandingPage'));
+const Signup = React.lazy(() => import('./components/Signup/Signup'));
+const Login = React.lazy(() => import('./components/Login/Login'));
 
 function App() {
   return (
     <div className="App">
+      <Suspense fallback={
+        <div className='centered'>Loading...</div>
+      }>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ Front-End/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import Navigationbar from '../Navigationbar/Navigationbar'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navigationbar />
+            <h1>Landing Page</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
