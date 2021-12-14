ac31f9d6ae45fb7a6c2515d0ce55347aea317c5a
Shubham <shubhamdutta1411@gmail.com>
2021-12-14 13:51:19 -0800

    Changes in Vaccine

--- Front-End/src/components/Vaccine/Vaccine.js
+++ Front-End/src/components/Vaccine/Vaccine.js
@@ -1,11 +1,11 @@
-import {Form, Button, Container, Col, Row, Figure} from "react-bootstrap";
+import { Form, Button, Container, Col, Row, Figure } from "react-bootstrap";
 import Navigationbar from "../Navigationbar/Navigationbar";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import {toast, ToastContainer} from "react-toastify";
-import {CreateVaccine, GetDisease} from "../../services";
-import {vaccineSliceActions} from "../../store/vaccineSlice";
-import {Navigate} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { CreateVaccine, GetDisease } from "../../services";
+import { vaccineSliceActions } from "../../store/vaccineSlice";
+import { Navigate } from "react-router-dom";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
 import Select from "react-select";
 import axios from "axios";
@@ -95,19 +95,15 @@ const Vaccine = () => {
       .get(`http://localhost:8080/disease`)
       .then((data) => {
         debugger;
-        e.preventDefault();
-        dispatch(CreateVaccine(vaccine));
-        dispatch(vaccineSliceActions.setError(""));
-    };
-
-    const onChange = (e) => {
-        //Add Create Vaccine
+        // const dropDownValue = data.data.map((response) => ({
+        //   key: response.diseaseId,
+        //   label: response.name,
+        // }));
+        setDiseases(data.data);
+        //diseases = dropDownValue;
+      })
+      .catch((err) => {
         debugger;
-    };
-
-    const getAllDiseases = () => {
-        debugger;
-<<<<<<< HEAD
         console.log(err);
       });
   };
@@ -249,142 +245,6 @@ const Vaccine = () => {
       </div>
     </div>
   );
-=======
-        axios
-            .get(`http://localhost:8080/disease`)
-            .then((data) => {
-                debugger;
-                // const dropDownValue = data.data.map((response) => ({
-                //   key: response.diseaseId,
-                //   label: response.name,
-                // }));
-                setDiseases(data.data);
-                //diseases = dropDownValue;
-            })
-            .catch((err) => {
-                debugger;
-                console.log(err);
-            });
-    };
-
-    const optionsArray = [
-        {key: "au", label: "Australia"},
-        {key: "ca", label: "Canada"},
-        {key: "us", label: "USA"},
-        {key: "pl", label: "Poland"},
-        {key: "es", label: "Spain"},
-        {key: "fr", label: "France"},
-    ];
-
-    return (
-        <div>
-            {!user && <Navigate to="/"/>}
-            <Navigationbar/>
-            <ToastContainer/>
-            <div className="container">
-                <Container>
-                    <div>
-                        <Row>
-                            <Col>
-                                <div id="login-image">
-                                    <Figure.Image
-                                        src={`${window.location.origin}/vaccine2.jpg`}
-                                        alt="Disease"
-                                    />
-                                </div>
-                            </Col>
-                            <Col>
-                                <Form id="login-form" onSubmit={onSubmit}>
-                                    <h1>Add Vaccine</h1>
-                                    <Form.Group className="loginbox" controlId="formEmail">
-                                        <Form.Label>Enter Vaccine name</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            name="name"
-                                            placeholder="Enter Vaccine Name"
-                                            required
-                                            onChange={(e) =>
-                                                setVaccine({...vaccine, name: e.target.value})
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <Form.Group className="loginbox" controlId="formEmail">
-                                        <Form.Label>Disease</Form.Label>
-                                        <DropdownMultiselect
-                                            handleOnChange={onChange}
-                                            options={diseases.map((e) => ({
-                                                label: e.name,
-                                                key: e.diseaseId,
-                                            }))}
-                                        />
-                                    </Form.Group>
-                                    <Form.Group className="loginbox" controlId="formPassword">
-                                        <Form.Label>Enter Vaccine Manufacturer</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            name="manufacturer"
-                                            placeholder="Enter Vaccine Manufacturer"
-                                            required
-                                            onChange={(e) =>
-                                                setVaccine({...vaccine, manufacturer: e.target.value})
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <Form.Group className="loginbox" controlId="formPassword">
-                                        <Form.Label>Number of Shots</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            name="numShots"
-                                            placeholder="Number of Shots"
-                                            required
-                                            onChange={(e) =>
-                                                setVaccine({
-                                                    ...vaccine,
-                                                    number_of_shots: e.target.value,
-                                                })
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <Form.Group className="loginbox" controlId="formPassword">
-                                        <Form.Label>Shot Interval</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            name="shotInterval"
-                                            placeholder="Shot Interval"
-                                            required
-                                            onChange={(e) =>
-                                                setVaccine({
-                                                    ...vaccine,
-                                                    shot_interval_val: e.target.value,
-                                                })
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <Form.Group className="loginbox" controlId="formPassword">
-                                        <Form.Label>Vaccine Validity</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            name="manufacturer"
-                                            placeholder="Vaccine Validity"
-                                            required
-                                            onChange={(e) =>
-                                                setVaccine({...vaccine, duration: e.target.value})
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <br/>
-                                    <Button id="loginbutton" type="submit">
-                                        Create Vaccine
-                                    </Button>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </div>
-                </Container>
-            </div>
-        </div>
-    );
->>>>>>> 3e1a771090019666db08b8387ad376d12da4bf0d
 };
 
 export default Vaccine;
